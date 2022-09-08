83624de659b02d354b8cf3b20a54212d398b0241
ihccenen <wirkz01@gmail.com>
2022-09-08 16:50:00 -0300

    Changed hover and click highlight color

--- script/script.js
+++ script/script.js
@@ -3,14 +3,14 @@ function mouseoverHighlight(e) {
     e.target.addEventListener('mouseout', () => e.target.classList.remove('key-hover'));
 }
 
-function click(e) {
+function mousedown(e) {
     e.target.classList.add('click');
     e.target.addEventListener('mouseup', () => setTimeout(() => {
         e.target.classList.remove('click')
     }, 100));
 }
 
-const numbers = document.querySelector('.numbers');
+const numbers = document.querySelector('.numbers-display');
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('mouseover', mouseoverHighlight));
-keys.forEach(key => key.addEventListener('mousedown', click));
+keys.forEach(key => key.addEventListener('mousedown', mousedown));
