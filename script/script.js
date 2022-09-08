68707a9617548fcb3e5738d3e95ba981ea91e121
ihccenen <wirkz01@gmail.com>
2022-09-08 15:50:42 -0300

    Added classes to the keys

--- script/script.js
+++ script/script.js
@@ -3,13 +3,14 @@ function mouseoverHighlight(e) {
     e.target.addEventListener('mouseout', () => e.target.classList.remove('key-hover'));
 }
 
-function clickHighlight(e) {
+function click(e) {
     e.target.classList.add('click');
     e.target.addEventListener('mouseup', () => setTimeout(() => {
         e.target.classList.remove('click')
     }, 100));
 }
 
+const numbers = document.querySelector('.numbers');
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('mouseover', mouseoverHighlight));
-keys.forEach(key => key.addEventListener('mousedown', clickHighlight));
+keys.forEach(key => key.addEventListener('mousedown', click));
