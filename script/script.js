510ee804bf57fc0036b315aa7c9994f23ae92a9b
ihccenen <wirkz01@gmail.com>
2022-09-08 15:31:40 -0300

    Changed highlight

--- script/script.js
+++ script/script.js
@@ -1,23 +1,15 @@
 function mouseoverHighlight(e) {
     e.target.classList.add('key-hover');
-}
-
-function removeHighlight(e) {
-    e.target.classList.remove('key-hover');
+    e.target.addEventListener('mouseout', () => e.target.classList.remove('key-hover'));
 }
 
 function clickHighlight(e) {
-    e.target.classList.remove('key-hover')
     e.target.classList.add('click');
-
-    setTimeout(() => {
-        e.target.classList.remove('click');
-    }, 100);
-
-    e.target.classList.add('key-hover');
+    e.target.addEventListener('mouseup', () => setTimeout(() => {
+        e.target.classList.remove('click')
+    }, 100));
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('mouseover', mouseoverHighlight));
-keys.forEach(key => key.addEventListener('mouseout', removeHighlight))
-keys.forEach(key => key.addEventListener('click', clickHighlight));
+keys.forEach(key => key.addEventListener('mousedown', clickHighlight));
