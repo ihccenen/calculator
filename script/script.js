fcfd4afd0a6a24ddc173f5c5a4e05a7d96a0c9de
ihccenen <wirkz01@gmail.com>
2022-09-10 14:40:31 -0300

    Fixed remove leading zero and added backspace

--- script/script.js
+++ script/script.js
@@ -8,25 +8,27 @@ function mouseoverHighlight(e) {
     e.target.addEventListener('mouseout', () => e.target.classList.remove('key-hover'));
 }
 
-function mousedown(e) {
+function mousedownHighlight(e) {
     e.target.classList.add('click');
     e.target.addEventListener('mouseup', () => setTimeout(() => {
         e.target.classList.remove('click');
     }, 100));
 }
 
-function removeLeadingZero(num) {
-    const hasDot = num.split('.')
-                    .map(num => num > 0 ? num - 0 : num)
-                    .join('.');
-    const displayNumber = num.indexOf('.') === -1 ? num - 0 : hasDot;
-    
-    return displayNumber;
+function removeLeadingZero(num) {           
+    if(num.indexOf('.') !== -1) {
+        let hasDot = num.split('.');
+        hasDot[0] - 0;
+        return  hasDot.join('.');
+    } else {
+        return num - 0;
+    } 
 }
 
 function getNumber(e) {
     const classNumber = e.target.classList.contains('number');
     const dot = e.target.classList.contains('dot') && numberDisplay.textContent.indexOf('.') === -1;
+    const backspace = e.target.classList.contains('backspace');
     const maxLength = numberDisplay.textContent.length < 9;
     let firstNumber = numberDisplay.textContent;
 
@@ -37,18 +39,26 @@ function getNumber(e) {
             firstNumber += e.target.textContent;
         }
     }
-    
-    numberDisplay.textContent = removeLeadingZero(firstNumber);    
+
+    if(backspace) {
+        firstNumber = numberDisplay.textContent.slice(0, -1)
+    }
+
+    numberDisplay.textContent = removeLeadingZero(firstNumber);
+
 }
 
 const bod = document.querySelector('body');
 const keys = Array.from(document.querySelectorAll('.key'));
 const numberDisplay = document.querySelector('.numbers-display');
+const calculateNumbers = {};
+let firstNumber = 0;
+let secondNumber = 0;
 
 bod.style.height = window.innerHeight + 'px';
 bod.style.width = window.innerWidth + 'px';
 window.addEventListener('resize', windowSize);
 keys.forEach(key => key.addEventListener('mouseover', mouseoverHighlight));
-keys.forEach(key => key.addEventListener('mousedown', mousedown));
+keys.forEach(key => key.addEventListener('mousedown', mousedownHighlight));
 keys.forEach(key => key.addEventListener('mouseup', getNumber));
-numberDisplay.textContent = 0;
+numberDisplay.textContent = firstNumber;
