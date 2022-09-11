f677b43021e8492cd2ccd86e460bfeb351797431
ihccenen <wirkz01@gmail.com>
2022-09-11 14:05:48 -0300

    Added getNumber and calculator

--- script/script.js
+++ script/script.js
@@ -15,13 +15,69 @@ function mousedownHighlight(e) {
     }, 100));
 }
 
+function calculator (firstNumber, operator, secondNumber) {
+    const a = +firstNumber;
+    const op = operator;
+    const b = +secondNumber;
+
+    switch (op) {   
+        case '+':
+            return a + b;
+        case '-':
+            return  a - b;
+        case '*':
+            return a * b;
+        case '÷':
+            return a / b;
+    }
+}
+
+function getNumber(e) {
+    if(a && op && b && (e.target.classList.contains('operator') || e.target.classList.contains('equal'))) {
+        const result = calculator(a, op, b);
+        numberDisplay.textContent = result;
+        a = result;
+        b = 0;
+        console.log(result);
+    }
+
+    if(a && op && (e.target.classList.contains('number') || e.target.classList.contains('dot'))) {
+        numberDisplay.textContent = 0;
+        b += e.target.textContent;
+        if(b.indexOf('.') === -1) {
+            b = +b;
+        }
+        numberDisplay.textContent = b;
+        console.log(b);
+    } else if(e.target.classList.contains('number')) {
+        if(numberDisplay.textContent.indexOf('.') === -1) {
+            numberDisplay.textContent = +numberDisplay.textContent + +e.target.textContent;
+        } else {
+            numberDisplay.textContent += e.target.textContent;
+        }
+    } else if(e.target.classList.contains('operator')) {
+        if(!a) {
+            a = numberDisplay.textContent;
+            console.log(a);
+        }
+        op = e.target.textContent;
+        console.log(op);
+    } if(e.target.classList.contains('dot') && numberDisplay.textContent.indexOf('.') === -1) {
+        numberDisplay.textContent += e.target.textContent;
+    }
+}
+
 const bod = document.querySelector('body');
 const keys = Array.from(document.querySelectorAll('.key'));
 const numberDisplay = document.querySelector('.numbers-display');
+let a;
+let op;
+let b = 0;
 
 bod.style.height = window.innerHeight + 'px';
 bod.style.width = window.innerWidth + 'px';
 window.addEventListener('resize', windowSize);
 keys.forEach(key => key.addEventListener('mouseover', mouseoverHighlight));
 keys.forEach(key => key.addEventListener('mousedown', mousedownHighlight));
+keys.forEach(key => key.addEventListener('mouseup', getNumber));
 numberDisplay.textContent = 0;
