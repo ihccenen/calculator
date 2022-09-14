1ac84184857ed9de2ef9070468ca449a453b9a82
ihccenen <wirkz01@gmail.com>
2022-09-14 19:19:51 -0300

    Added comments

--- script/script.js
+++ script/script.js
@@ -34,6 +34,8 @@ function getSecondNumber(itsNumber, itsDot, text) {
         calcArray[2] += text;
     } else if (itsNumber && maxLength) {
         calcArray[2] += text;
+
+        // remove leading zeros if there's no dot
         calcArray[2] = calcArray[2].indexOf('.') === -1 ? calcArray[2] - 0 : calcArray[2];
     }
 
@@ -49,8 +51,12 @@ function getFirstNumber(itsNumber, itsDot, text) {
         numberDisplay.textContent += text;
     } else if(itsNumber && maxLength) {
         numberDisplay.textContent += text;
+
+        // remove leading zeros if there's no dot
         numberDisplay.textContent = numberDisplay.textContent.indexOf('.') === -1 ? numberDisplay.textContent - 0 : numberDisplay.textContent;
     } else if(itsOperator) {
+
+        // save first number and operator
         calcArray[0] = numberDisplay.textContent;
         calcArray[1] = text;
     }
@@ -59,27 +65,37 @@ function getFirstNumber(itsNumber, itsDot, text) {
 function clearDisplay(itsClear, itsBackspace) {
     if(numberDisplay.textContent.length > 1 && itsBackspace) {
         numberDisplay.textContent = numberDisplay.textContent.slice(0, -1);
+
+        // remove last character if it's the second number
         calcArray[2] = calcArray[2] > 0 ? numberDisplay.textContent : calcArray[0];
     } else if(itsBackspace) {
         numberDisplay.textContent = 0;
+
+        // remove last character if it's the second number
         calcArray[2] = calcArray[2] > 0 ? numberDisplay.textContent : calcArray[0];
     } else if(itsClear) {
         numberDisplay.textContent = 0;
         [calcArray[0], calcArray[1], calcArray[2]] = [0, false, 0];
     }
 
+    // if the last character is + or - change it to 0
     numberDisplay.textContent = isNaN(numberDisplay.textContent) ? 0 : numberDisplay.textContent;
 }
 
 function getPercent() {
     numberDisplay.textContent /= 100;
-
+    
+    // if it's used on the second number 
     if(calcArray[2] !== 0) {
+        // calculate from the first number and subtract
         numberDisplay.textContent = calcArray[0] - (calcArray[0] * (calcArray[2] / 100));
     }
 }
 
 function getInput(e) {
+    e.preventDefault();
+
+    // check which class the div clicked or key pressed has
     const hasNum = e.target.classList.contains('number') || e.key >= 0;
     const hasDot = e.target.classList.contains('dot') || e.key === '.';
     const hasEquals = e.target.classList.contains('equals') || e.key === '=' || e.key === 'Enter';
@@ -88,40 +104,47 @@ function getInput(e) {
     const hasPercent = e.target.classList.contains('percent') || e.key === '%';
     const hasClear = e.target.classList.contains('clear');
     const hasPlusMinus = e.target.classList.contains('plus-minus');
+
+    // set conditions
+    const firstNumber = hasNum || hasDot || hasOp;
     const result = calcArray[0] && calcArray[1] && calcArray[2] && (hasOp || hasEquals);
     const secondNumber = calcArray[0] && calcArray[1] && (hasNum || hasDot);
-    const firstNumber = hasNum || hasDot || hasOp;
-
-    if(result) {
-        const a = calculator(calcArray);
-        numberDisplay.textContent = a;
-        [calcArray[0], calcArray[1], calcArray[2]] = [a, false, 0];
-    }
     
-    if(secondNumber) {
-        const text = e.key || e.target.textContent;
+    // get text from the div clicked or the key pressed
+    const text = e.key || e.target.textContent;
+
+    // run the one which is true of result, firstNumber or secondNumber
+    // or the one which has the right class or key pressed
+    if(result) {
+        const total = calculator(calcArray);
+        numberDisplay.textContent = total;
+
+        // set calcArray to be: total, text if it's an operator and 0
+        [calcArray[0], calcArray[1], calcArray[2]] = [total, text !== '=', 0];
+    } else if(secondNumber) {
         getSecondNumber(hasNum, hasDot, text);
     } else if(firstNumber) {
-        const text = e.key || e.target.textContent;
         getFirstNumber(hasNum, hasDot, text);
     } else if(hasBackspace || hasClear) {
         clearDisplay(hasClear, hasBackspace);
     } else if(hasPercent) {
         getPercent();
     } else if(hasPlusMinus) {
+        // convert number to negative or positive
         numberDisplay.textContent *= -1;
+
+        // if it's the second number
         calcArray[2] = calcArray[2] !== 0 ? numberDisplay.textContent : 0;
     }
 
-
-    if(hasNum || hasDot || hasOp || hasBackspace || hasEquals || hasPercent) {
-        e.preventDefault();
-    }
+    console.log(calcArray);
 }
 
 const bod = document.querySelector('body');
 const keys = Array.from(document.querySelectorAll('.key'));
 const numberDisplay = document.querySelector('.numbers-display');
+
+// store first number, operator and second number
 const calcArray = [0, false, 0];
 
 bod.style.height = window.innerHeight + 'px';
